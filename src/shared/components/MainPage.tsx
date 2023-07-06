116aad6139710c99ee303e1694c2680e4b97d8dd
Andre Vega <andrevega@Andres-MacBook-Pro.local>
2023-07-05 20:35:44 -0600

    fix: corrections on header, information and footer from design review session

--- src/shared/components/MainPage.tsx
+++ src/shared/components/MainPage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import "./mainPage.styles.scss";
 import Body from "./Body";
+import Footer from "./Footer";
 
 const MainPage = () => {
   return (
     <div className="main-div">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 };
