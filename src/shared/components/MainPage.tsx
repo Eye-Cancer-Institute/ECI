238cfcbb941a0daf5af5101c86f6969841443441
Andre Vega <andrevega@Andres-MacBook-Pro.local>
2023-07-05 23:55:08 -0600

    feat: Cuestionario and Checkbox reusable component

--- src/shared/components/MainPage.tsx
+++ src/shared/components/MainPage.tsx
@@ -3,12 +3,14 @@ import Header from "./Header";
 import "./mainPage.styles.scss";
 import Body from "./Body";
 import Footer from "./Footer";
+import Cuestionario from "./Cuestionario";
 
 const MainPage = () => {
   return (
     <div className="main-div">
       <Header />
       <Body />
+      <Cuestionario />
       <Footer />
     </div>
   );
---
+++ src/shared/components/reusable/CustomCheckbox/Checkbox.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Checkbox.styles.scss";
+
+interface CheckboxProps {
+  isChecked: boolean;
+  setIsChecked: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function Checkbox({ isChecked, setIsChecked }: CheckboxProps) {
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div
+      className={`custom-checkbox ${isChecked ? "checked" : ""}`}
+      onClick={handleCheckboxChange}
+    >
+      <div className={`checkbox-inner ${isChecked ? "checked" : ""}`}></div>
+    </div>
+  );
+}
+
+export default Checkbox;
